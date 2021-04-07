50794ae02e41357e375c832d2f6cec59c2b540ba
Muhammad Taha Khilji <muhammad.taha1993@gmail.com>
2021-04-07 15:50:27 +0500

    Version 1

--- DemoApp/MainWindow.xaml.cs
+++ DemoApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,7 +27,7 @@ namespace DemoApp
             InitializeComponent();
             dataGridView.DataContext = loadData();
         }
-
+        int colIndex, rowIndex;
        DataTable loadData()
         {
             DataTable dt = new DataTable();
@@ -38,13 +39,42 @@ namespace DemoApp
             dt.Rows.Add(new object[] { "Bucky, Winter Soldier", 800, "Hydra HQ" });
             dt.Rows.Add(new object[] { "Natasha, Black Widow", 700, "Sheild HQ",});
             return dt;
-           
+        }
+    
+
+        private void GetRowandColumnIndex(object sender, RoutedEventArgs e)
+        {
+            DataGridCell x = (DataGridCell)sender;
+            colIndex = x.Column.DisplayIndex;
+            rowIndex = DataGridRow.GetRowContainingElement(x).GetIndex();
+            txtColumn.Text = colIndex.ToString();
+            txtRow.Text = rowIndex.ToString();
+        }
+
+        string RequiredMethod()
+        {
+            colIndex = Convert.ToInt32(txtColumn.Text);
+            rowIndex = Convert.ToInt32(txtRow.Text);
+            var dt = ConverttoTable();
+            return dt.Rows[rowIndex][colIndex].ToString();
         }
 
 
-        DataGridCell RequiredMethod()
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            return new DataGridCell();
+           
+            MessageBox.Show(RequiredMethod());
+        }
+
+         public  DataTable ConverttoTable()
+        {
+            DataView view = (DataView)dataGridView.ItemsSource;
+            DataTable dataTable = view.Table.Clone();
+            foreach(DataRowView v in view)
+            {
+                dataTable.Rows.Add(v.Row.ItemArray);
+            }
+            return dataTable;
         }
     }
 }
